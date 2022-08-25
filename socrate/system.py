756e4b1d81c139097e4d6b8b25027337b80d4ee7
Alexander Graf <ghostwheel42@users.noreply.github.com>
2022-08-25 16:34:34 +0200

    Simplify resolve_address again and prefer IPv4

--- socrate/system.py
+++ socrate/system.py
@@ -1,6 +1,5 @@
 import socket
 import tenacity
-import urllib.parse
 from os import environ
 import logging as log
 
@@ -9,11 +8,9 @@ import logging as log
 def resolve_hostname(hostname):
     """ This function uses system DNS to resolve a hostname.
     It is capable of retrying in case the host is not immediately available
-    It returns (Address, AddressFamily)
     """
     try:
-        res = socket.getaddrinfo(hostname, None)[0]
-        return (res[4][0], res[0])
+        return sorted(socket.getaddrinfo(hostname, None, socket.AF_UNSPEC, socket.SOCK_STREAM, 0, socket.AI_PASSIVE), key=lambda s:s[0])[0][4][0]
     except Exception as e:
         log.warn("Unable to lookup '%s': %s",hostname,e)
         raise e
@@ -23,17 +20,11 @@ def resolve_address(address):
     """ This function is identical to ``resolve_hostname`` but also supports
     resolving an address, i.e. including a port.
     """
-
-    # urlparse() and urlsplit() insists on absolute URLs starting with "//"
-    result = urllib.parse.urlsplit('//{}'.format(address))
-    hostname, port = result.hostname, result.port
-    
-    ip_address, address_family = resolve_hostname(hostname)
-
-    if address_family is socket.AF_INET6:
-        return "[{}]:{}".format(ip_address, port)
-
-    return "{}:{}".format(ip_address, port)
+    hostname, *rest = address.rsplit(":", 1)
+    ip_address = resolve_hostname(hostname)
+    if ":" in ip_address:
+        ip_address = "[{}]".format(ip_address)
+    return ip_address + "".join(":" + port for port in rest)
 
 
 def get_host_address_from_environment(name, default):
